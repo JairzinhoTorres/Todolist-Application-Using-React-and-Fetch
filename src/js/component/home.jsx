20efcc2d69ea91b2a0d81a944ef9590d11d4c68e
JairzinhoTorres <jair1924torres@gmail.com>
2023-01-19 13:04:33 +0000

    Funciona, pero hay un pequeño error en la consola... Solucionar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,118 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	
+	const [tarea,setTarea] = useState("");
+	const [lista,setLista] = useState([]);
+
+
+	/*const listItems = list.map((item) =>
+	<li key={item.toString()}>
+	{item}
+  </li>
+	);*/
+
+
+	function crearUsuario(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/jairtorres`,
+		{method: 'POST', 
+		headers: {
+			'Content-Type': 'application/json'},
+		body: JSON.stringify([])
+	  })
+		.then((response)=>response.json())
+		.then((data)=>console.log(data))
+	}
+
+
+	function agregarTarea(e) {
+		e.preventDefault()
+// {label: 'sample task', done: false}
+		// setLista([...lista, {tarea}]);
+		setLista(lista.concat({label: tarea, done: false}))
+		setTarea("")
+	}
+
+	const borrarTarea = (indexItem) => {
+		setLista((prevState) =>
+		  prevState.filter((listaItems, index) => index !== indexItem)
+		);
+	  };
+
+	function obtenerLista(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/jairtorres`,
+		{method: 'GET', 
+		
+	  })
+		.then((response)=>response.json())
+		.then((data)=>setLista(data))
+	}
+
+	function borrarLista(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/jairtorres`,
+		{method: 'DELETE', 
+		headers: {
+			'Content-Type': 'application/json'}
+	  })
+		.then((response)=>response.json())
+		.then((data)=>{
+			console.log(data.result)
+			if (data.result === "ok"){
+				setLista([])
+			}
+		})
+		
+	}
+
+	function actualizar(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/jairtorres`,
+		{method: 'PUT', 
+		headers: {
+			'Content-Type': 'application/json'},
+		body: JSON.stringify(lista)
+	  })
+		.then((response)=>response.json())
+		.then((data)=>console.log(data))
+	}
+
+	
+	useEffect (()=>{
+		crearUsuario();
+		obtenerLista()
+	},[])
+
+	useEffect (()=>{
+		actualizar()
+		},[lista])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="card container d-flex bg-light mt-3 md-w50">
+		<h2 className="titulo m-auto p-2">Lista de tareas.</h2>
+  			<div className="card-body">
+  				<input type="text" className="input m-1 w-75" value={tarea} id="exampleInput" aria-describedby="inputHelp" onChange={(e)=>{setTarea(e.target.value)}} placeholder="Añadir una tarea."/>
+				<button type="submit" className="btn btn-primary btn-sm" onClick={agregarTarea}>Agregar</button>
+  			</div>
+			<div className="to-do-list d-flex">
+			  <ul>{lista.map((item, index) => (
+        <li key={index}>
+          {item.label}
+          <button className="btn" onClick={() => borrarTarea(index)}>
+            <i className="fas fa-trash-alt" />
+          </button>
+        </li>
+      ))}</ul>
+			</div>
+			<div className="delete-lista d-flex justify-content-center mt-3 md-w50 mb-2">
+			<button type="submit" className="btn btn-danger btn-sm" onClick={borrarLista}>Delete list</button>
+			</div>
 		</div>
+		
+		</>
 	);
 };
 
